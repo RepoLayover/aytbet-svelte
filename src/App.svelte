79138d530cc761c26bc626c5eab295bd83e8ba35
RepoLayover <133013673+RepoLayover@users.noreply.github.com>
2023-05-18 13:00:42 +0530

    [update] sveltewebapp

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>This is {name} or --> "Aight Bet"</h1>
+	<p>Visit the AI bot or helper at <a href="https://bruvai.com" target="_blank"> bruvAI </a> to learn more about this server and any help with your milestone achievement</p>
+	<p>Visit aytbet.com <a href="https://aytbet.com" target="_blank">aytbet.com</a> the aytbet.com home page</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: lowercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
